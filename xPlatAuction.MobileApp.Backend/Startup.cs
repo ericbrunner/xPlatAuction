b4ac85a30adbdd7994af2eb9b8439f85e34d9543
erich brunner <erich.brunner@rolsped.com>
2016-09-04 12:30:01 +0200

    added Mobile App Backend

---
+++ xPlatAuction.MobileApp.Backend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(xPlatAuction.MobileApp.Backend.Startup))]
+
+namespace xPlatAuction.MobileApp.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
